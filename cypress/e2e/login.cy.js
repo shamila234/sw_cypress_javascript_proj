a938d64ef019e9216ea6886317aa105e2e0447cd
Shamila Wijekoon <shamila234@gmail.com>
2024-03-01 12:17:28 +1300

    initial project setup

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,18 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.viewport(1536, 960)
+  });
+
+  it('Login', () => {
+    cy.visit('https://www.thewarehouse.co.nz/')
+
+    cy.get('twg-popover > .header-action-link').trigger('mouseover')
+    cy.get('[data-test-id=popover-signin-btn').click()
+    cy.get('#login-form-email').type('test-email@gmail.com')
+    cy.get('#login-form-password').type('test-password')
+    cy.get('.login-submit.mb-xl').click()
+
+    cy.get('#challenge-running').should('include.text', 'Verifying you are human')
+
+  })
+})
